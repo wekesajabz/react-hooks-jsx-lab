a0f0e822d3c390a769c7c15fd1e5361fde88f5d2
wekesajabz <wekesajabez6550@gmail.com>
2024-08-01 08:46:21 +0300

    added the about me, home and navbar contents

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return     <div id="about">
+  <h2>About Me</h2>
+  <p>This is a brief description about myself. I am passionate about web development and love to create engaging and interactive web applications.</p>
+  <img src={image} alt="I made this" />
+</div>;
 }
 
 export default About;
